af12ca50ca0cb550010188d4b67156fe4956963c
Justin AKOUETE <justinakouete42@gmail.com>
2026-06-06 14:08:04 +0200

    Fix assets CSS/JS production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,12 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @if(app()->environment('production'))
+    <link rel="stylesheet" href="/build/assets/app-CEwZte8_.css">
+    <script src="/build/assets/app-BjMeHjpC.js" defer></script>
+@else
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@endif
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
